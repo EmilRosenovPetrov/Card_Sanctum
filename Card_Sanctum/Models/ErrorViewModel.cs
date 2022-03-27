b764fd8e066cd5b7d6c955364e4f5b4165b843ab
Emil Petrov <emil.petrov2020@gmail.com>
2022-03-27 13:51:02 +0300

    Add project files.

---
+++ Card_Sanctum/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Card_Sanctum.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
