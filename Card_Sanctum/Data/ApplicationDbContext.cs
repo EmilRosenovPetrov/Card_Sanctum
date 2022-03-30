d546b465a4439cc275c5494a292d52f5fe69d626
Emil Petrov <emil.petrov2020@gmail.com>
2022-03-30 17:54:21 +0300

    Scafolded Identity.

--- Card_Sanctum/Data/ApplicationDbContext.cs
+++ Card_Sanctum/Data/ApplicationDbContext.cs
@@ -1,9 +1,10 @@
 ﻿namespace Card_Sanctum.Data
 {
+    using Card_Sanctum.Infrastructure.Data.Identity;
     using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
     using Microsoft.EntityFrameworkCore;
 
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
