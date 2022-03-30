622e91c13fb92c183bb81705fa6c46070a5b78eb
Emil Petrov <emil.petrov2020@gmail.com>
2022-03-30 16:10:50 +0300

    Custom ValidationAttribute IsBefore added.

---
+++ Card_Sanctum.Core/CustomAttributes/IsBeforeAttribute.cs
@@ -0,0 +1,36 @@
+﻿namespace Card_Sanctum.Core.CustomAttributes
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+
+    public class IsBeforeAttribute : ValidationAttribute
+    {
+        private readonly string propertyToCompare;
+
+        public IsBeforeAttribute(string _propertyToCompare, string errorMessage = "")
+        {
+            
+            this.ErrorMessage = errorMessage;
+            propertyToCompare = _propertyToCompare;
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            try
+            {
+                DateTime dateToCompare = (DateTime)validationContext
+                .ObjectType.GetProperty(propertyToCompare)
+                .GetValue(validationContext.ObjectInstance);
+
+                if ((DateTime)value < dateToCompare)
+                {
+                    return ValidationResult.Success;
+                }
+            }
+            catch (Exception)
+            {}
+
+            return new ValidationResult(ErrorMessage);
+        }
+    }
+}
