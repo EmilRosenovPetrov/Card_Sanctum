343b82ba20490110d796495d20bb8a44e7ef40f8
Emil Petrov <emil.petrov2020@gmail.com>
2022-04-02 18:55:36 +0300

    Added DbContext extension method.

--- Card_Sanctum/Extensions/ServiceCollectionExtension.cs
+++ Card_Sanctum/Extensions/ServiceCollectionExtension.cs
@@ -1,12 +1,24 @@
 ﻿namespace Microsoft.Extensions.DependencyInjection
 {
+    using Card_Sanctum.Infrastructure.Data;
     using Card_Sanctum.Infrastructure.Data.Common.Repository;
+    using Microsoft.EntityFrameworkCore;
 
     public static class ServiceCollectionExtension
     {
         public static IServiceCollection AddApplicationServices(this IServiceCollection services)
         {
             services.AddScoped<IRepository, Repository>();
+
+            return services;
+        }
+
+        public static IServiceCollection AddApplicationDbContexts(this IServiceCollection services, IConfiguration config)
+        {
+            var connectionString = config.GetConnectionString("DefaultConnection");
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(connectionString));
+            services.AddDatabaseDeveloperPageExceptionFilter();
 
             return services;
         }
