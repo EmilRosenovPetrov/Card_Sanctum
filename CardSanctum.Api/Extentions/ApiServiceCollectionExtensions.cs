8d39a1c059848cd1c2bc49f3a1a9c28b41ebcd50
Emil Petrov <emil.petrov2020@gmail.com>
2022-04-03 17:10:42 +0300

    Added Api Service extentions

---
+++ CardSanctum.Api/Extentions/ApiServiceCollectionExtensions.cs
@@ -0,0 +1,25 @@
+﻿namespace Microsoft.Extensions.DependencyInjection
+{
+    using Card_Sanctum.Infrastructure.Data;
+    using Card_Sanctum.Infrastructure.Data.Common.Repository;
+    using Microsoft.EntityFrameworkCore;
+
+    public static class ServiceCollectionExtension
+    {
+        public static IServiceCollection AddApiServices(this IServiceCollection services)
+        {
+            services.AddScoped<IRepository, Repository>();
+
+            return services;
+        }
+
+        public static IServiceCollection AddApiDbContexts(this IServiceCollection services, IConfiguration config)
+        {
+            var connectionString = config.GetConnectionString("DefaultConnection");
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(connectionString));
+
+            return services;
+        }
+    }
+}
