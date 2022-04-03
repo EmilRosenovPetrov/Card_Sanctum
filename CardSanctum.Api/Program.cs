904b49e43ea4b99e96002728b9ed2e7315f853b2
Emil Petrov <emil.petrov2020@gmail.com>
2022-04-03 17:12:13 +0300

    Removed weather forecast

--- CardSanctum.Api/Program.cs
+++ CardSanctum.Api/Program.cs
@@ -2,6 +2,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddApiDbContexts(builder.Configuration);
+builder.Services.AddApiServices();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
